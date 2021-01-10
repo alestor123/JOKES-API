e3913002f5bea042ccef0c1ca4ec9613692d895c
alestor123 <alestoraldous@gmail.com>
2021-01-10 13:28:22 +0530

    added main code

--- App.js
+++
@@ -1,5 +0,0 @@
-var axios = require('axios');
-module.exports = async (type) => {
-var data = await axios.get(`https://v2.jokeapi.dev/joke/${type}?type=single`)
-return data.data.joke
-}
--- cli.js
+++ cli.js
@@ -1,1 +1,6 @@
 #!/usr/bin/env node
+
+var jokes = require('./App');
+(async () => {
+console.log(await jokes(process.argv[2] || 'Programming'))
+})();
